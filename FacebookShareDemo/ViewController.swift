7f1e3dabdfe059788986f49a54e53d4e021de8c4
Dhanesh Gosai <dgosai@cisco.com>
2019-08-02 18:12:31 +0530

    facebook changes

--- FacebookShareDemo/ViewController.swift
+++ FacebookShareDemo/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import FBSDKShareKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , SharingDelegate {
+    func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
+        print(results)
+    }
+    
+    func sharer(_ sharer: Sharing, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func sharerDidCancel(_ sharer: Sharing) {
+        print("Share Canceled")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func  btnFBShareClick (_ sender: AnyObject) {
+        faceBookShare()
+    }
+    
+    func faceBookShare()
+    {
+        
+        let content = ShareLinkContent()
+        content.contentURL = URL(string: "https://developers.facebook.com")!
+//        FBSDKShareDialog.show(from: self, with: content, delegate: self)
+        
+//        let content:ShareLinkContent = ShareLinkContent.init(url: URL.init(string: "https://developers.facebook.com") ?? URL.init(fileURLWithPath: "https://developers.facebook.com"), quote: "Share This Content")
+        let sharedialog = ShareDialog.init(fromViewController: self, content: content, delegate: self)
+        sharedialog.show()
     }
 
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FacebookShareDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FacebookShareDemo
+pod 'FacebookCore'
+pod 'FacebookLogin'
+pod 'FacebookShare'
+
+end
+
+#post_install do |installer|
+#  installer.pods_project.targets.each do |target|
+#    target.build_configurations.each do |config|
+#      config.build_settings['DYLIB_COMPATIBILITY_VERSION'] = ''
+#    end
+#  end
+#end
